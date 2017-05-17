7e24ab04286e975fdb62f75a2292c8d6914ba385
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-05-17 21:29:47 +0300

    Remove uneed themes; translate comments

--- modules/themes-module.el
+++ modules/themes-module.el
@@ -5,35 +5,24 @@
 ;; themes
 (setq custom-safe-themes t)
 ;;(load-theme 'zenburn t)
-;;(load-theme 'phoenix-dark-mono t)
-;;(load-theme 'ample t t)
-;(enable-theme 'ample)
-(add-to-list 'custom-theme-load-path
-             "~/.emacs.d/themes/")
-;;(load-theme 'minimal)
+
 (load-theme 'sexy-monochrome t)
 ;;(enable-theme 'sexy-monochrome)
-;;(load-theme 'tomorrow-night-paradise t)
-;;(load-theme 'base16-solarized-dark t)
-;;(load-theme 'ujelly t)
-;;(enable-theme 'ujelly)
+
 (defun cursor-active-mark-init ()
-    "Данная функция меняет цвет курсора на красный."
-(set-cursor-color "#ff0000")
-(setq cursor-type 'bar))
-"Мы можем дописывать нашу функцию, например, нам нужно, чтобы эта функция также меняла сам курсор на hollow:."
+    "Change cursor color into red in mark mode."
+    (set-cursor-color "#ff0000")
+    (setq cursor-type 'bar))
 
 (add-hook 'activate-mark-hook 'cursor-active-mark-init)
-"Теперь напишем хук, при котором вызывается наша функция."
+"Hook that call to change cursor color function."
+
 (defun th-deactivate-mark-init ()
-  "Когда мы выделяем текст, наш курсор становится красным и имеет стиль bar.
-Но есть проблема, даже если мы закончим выделять текст,
-стиль на дефолтный не вернётся.
-Поэтому пишем функцию и хук, которые делают это."
-(set-cursor-color "light gray")
-(setq cursor-type 'box))
+    "Hook that change back cursor to normal color, after mark mode is done."
+    (set-cursor-color "light gray")
+    (setq cursor-type 'box))
 
 (add-hook 'deactivate-mark-hook 'th-deactivate-mark-init)
-"Если выделение текста не активно."
+"If mark-mode is off, do nothing"
 (provide 'themes-module)
 ;;; themes-module ends here
