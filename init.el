f3a515d47d8d4879f4e8e587da24feb5c8c597e2
Volodymyr Yevtushenko <v.yevtushenko@tickets.ua>
2017-09-19 10:45:57 +0300

    Change dump-jump keybindings

--- init.el
+++ init.el
@@ -378,6 +378,15 @@
            ("C-<f6>" . bm-toggle)
            ("C-c C-<f6>" . bm-show-all)))
 
+(use-package dumb-jump
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g i" . dumb-jump-go-prompt)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window))
+  :config (setq dumb-jump-selector 'ivy) ;; (setq dumb-jump-selector 'helm)
+  :ensure)
+
 (use-package dump-jump
     :init
     (dumb-jump-mode)
