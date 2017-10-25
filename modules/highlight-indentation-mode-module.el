2f1f7c01bc701e18a9738bf8be859b4292748c6f
Volodymyr Yevtushenko <v.yevtushenko@tickets.ua>
2017-10-25 10:54:01 +0300

    Change settiings of smartparens and highlight-guide

--- modules/highlight-indentation-mode-module.el
+++ modules/highlight-indentation-mode-module.el
@@ -2,23 +2,12 @@
 ;;; Code:
 ;;; Commentary:
 ;; highlight indentation
-(highlight-indentation-current-column-mode 1)
-;;(provide 'init)
-;;add highlight ingentation
-(global-set-key(kbd "<f9>") 'highlight-indentation-current-column-mode)
-(set-face-background 'highlight-indentation-face "grey9")
-(set-face-background 'highlight-indentation-current-column-face "grey9")
-;;hooks ;)
-;;; hooks for highlightion
-(add-hook 'ruby-mode-hook 'highlight-indentation-current-column-mode)
-(add-hook 'python-mode-hook 'highlight-indentation-current-column-mode)
-(add-hook 'haml-mode-hook 'highlight-indentation-current-column-mode)
-(add-hook 'web-mode-hook 'highlight-indentation-current-column-mode)
-(add-hook 'rust-mode-hook 'highlight-indentation-current-column-mode)
-(add-hook 'lisp-mode-hook 'highlight-indentation-current-column-mode)
-(add-hook 'emacs-lisp-mode-hook 'highlight-indentation-current-column-mode)
-(add-hook 'yml-mode-hook 'highlight-indentation-current-column-mode)
-(add-hook 'slim-mode-hook 'highlight-indentation-current-column-mode)
-(add-hook 'c-mode-hook 'highlight-indentation-current-column-mode)
+(use-package highlight-indentation-current-column-mode
+    :ensure t
+    :bind
+    ("<f9>" . highlight-indentation-current-column-mode)
+    :config
+    (set-face-background 'highlight-indentation-face "grey9")
+    (set-face-background 'highlight-indentation-current-column-face "grey9"))
 (provide 'highlight-indentation-mode-module)
 ;;; highlight-indentation-mode-module ends here
--- modules/smartparens-module.el
+++ modules/smartparens-module.el
@@ -1,36 +1,40 @@
 ;;; package --- Summary
 ;;; Commentary:
 ;; Default setup of smartparens
-(require 'smartparens-config)
 ;;; Code:
-(add-hook 'js2-mode-hook 'smartparens-mode)
-(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
-(add-hook 'web-mode-hook 'smartparens-mode)
 
-(--each '(restclient-mode-hook
-          js-mode-hook
-          js2-mode-hook
-          python-mode-hook
-          web-mode-hook
-          ruby-mode-hook
-          markdown-mode-hook
-          org-mode-hook
-          rust-mode-hook
-          cc-mode-hook
-          lisp-mode-hook
-          emacs-lisp-mode-hook
-          haml-mode-hook
-          c-mode-hook
-          go-mode-hook
-          elixir-mode-hook
-          enh-ruby-mode-hook
-          crystal-mode-hook
-          slim-mode-hook
-          haml-mode-hook
-          rust-mode-hook
-          yaml-mode-hook
-          nginx-mode-hook
-          scss-mode-hook)
-    (add-hook it 'turn-on-smartparens-mode))
+(use-package smartparens
+    :config
+    (show-smartparens-global-mode 1)
+
+    (require 'smartparens-config)
+    (add-hook 'js2-mode-hook 'smartparens-mode)
+    (add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+    (add-hook 'web-mode-hook 'smartparens-mode)
+    (--each '(restclient-mode-hook
+              js-mode-hook
+              js2-mode-hook
+              python-mode-hook
+              web-mode-hook
+              ruby-mode-hook
+              markdown-mode-hook
+              org-mode-hook
+              rust-mode-hook
+              cc-mode-hook
+              lisp-mode-hook
+              emacs-lisp-mode-hook
+              haml-mode-hook
+              c-mode-hook
+              go-mode-hook
+              elixir-mode-hook
+              enh-ruby-mode-hook
+              crystal-mode-hook
+              slim-mode-hook
+              haml-mode-hook
+              rust-mode-hook
+              yaml-mode-hook
+              nginx-mode-hook
+              scss-mode-hook)
+        (add-hook it 'turn-on-smartparens-mode)))
 (provide 'smartparens-module)
 ;;; smartparens-module ends here
