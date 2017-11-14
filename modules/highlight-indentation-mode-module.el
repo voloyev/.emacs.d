41c1317dec70278c5b3d23e04b008c67d79a98a3
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2017-11-14 22:39:44 +0200

    Add highlight indentation color

--- modules/highlight-indentation-mode-module.el
+++ modules/highlight-indentation-mode-module.el
@@ -2,11 +2,11 @@
 ;;; Code:
 ;;; Commentary:
 ;; highlight indentation
-(use-package highlight-indentation-mode
+(use-package highlight-indentation
     :ensure t
     :bind
     ("<f9>" . highlight-indentation-mode)
-    :config
+    :init
     (set-face-background 'highlight-indentation-face "grey20"))
 (provide 'highlight-indentation-mode-module)
 ;;; highlight-indentation-mode-module ends here
