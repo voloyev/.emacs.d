a01e8d382a9bc6c1c56f6eb9fb2a3c9007d22fd7
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2017-11-22 15:31:04 +0200

    Js indentation

--- modules/js-module.el
+++ modules/js-module.el
@@ -8,6 +8,13 @@
 (custom-set-variables '(js2-basic-offset 2))
 (custom-set-variables '(js-basic-offset 2))
 (custom-set-variables '(jsx-basic-offset 2))
+(custom-set-variables '(vue-basic-offset 2))
+(setq js-indent-level 2)
+(setq js2-indent-level 2)
+(setq vue-indent-level 2)
+
+(use-package vue-mode
+    :ensure t)
 
 (provide 'js-module)
 ;;; js-module.el ends here
