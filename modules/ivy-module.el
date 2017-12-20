09e792cb7ea83ec97052abe4605ea053b3ea3d0c
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2017-12-20 11:39:42 +0200

    Change counsel projectile setup

--- modules/ivy-module.el
+++ modules/ivy-module.el
@@ -5,13 +5,16 @@
 (use-package ivy
     :ensure t
     :init
-    (counsel-projectile-on)
+    (counsel-projectile-mode)
     :config
     (setq ivy-use-virtual-buffers t)
     (setq ivy-count-format "(%d/%d) ")
     :bind(("C-s" . swiper)
           ("C-c m g" . counsel-git-grep)
           ("C-c m a" . counsel-projectile-ag)))
+
+(use-package counsel-projectile
+    :ensure t)
 
 (provide 'ivy-module)
 ;;; ivy-module.el ends here
