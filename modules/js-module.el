7c638a6f97a4b5f5b4de8653a0d9d80fe086072a
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-02-13 10:16:37 +0200

    Add mode for typescript

--- modules/js-module.el
+++ modules/js-module.el
@@ -13,8 +13,32 @@
 (setq js2-indent-level 2)
 (setq vue-indent-level 2)
 
+;; vue mode
 (use-package vue-mode
     :ensure t)
 
+;; typescript
+(use-package tide
+    :ensure t)
+
+(defun setup-tide-mode ()
+    (interactive)
+    (tide-setup)
+    (flycheck-mode +1)
+    (setq flycheck-check-syntax-automatically '(save mode-enabled))
+    (eldoc-mode +1)
+    (tide-hl-identifier-mode +1)
+    ;; company is an optional dependency. You have to
+    ;; install it separately via package-install
+    ;; `M-x package-install [ret] company`
+    (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
 (provide 'js-module)
 ;;; js-module.el ends here
