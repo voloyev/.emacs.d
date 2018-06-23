8a3df2231571045dcd406fc96ce1fd06a96adcc2
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-06-24 00:05:02 +0300

    Add ivy popup

--- modules/ivy-module.el
+++ modules/ivy-module.el
@@ -14,5 +14,13 @@
 (use-package counsel-projectile
     :ensure t)
 
+(use-package ivy-posframe
+    :ensure t)
+
+(setq ivy-display-function #'ivy-posframe-display-at-frame-bottom-window-center)
+(push '(t . ivy-posframe-display) ivy-display-functions-alist)
+
+(ivy-posframe-enable)
+
 (provide 'ivy-module)
 ;;; ivy-module.el ends here
