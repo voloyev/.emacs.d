a0804ca70715d72b36da22411fd9de32ea580c7c
Volodymyr Yevtushenko <volodymyr_yevtushenko@epam.com>
2018-06-27 12:40:55 +0300

    avy update keybindings

--- modules/avy-module.el
+++ modules/avy-module.el
@@ -5,7 +5,7 @@
 (use-package avy
     :ensure t
     :bind(("C-;"        . avy-goto-char)
-          ("C-c g c 2"  . avy-goto-char-2)
+          ("C-:"  . avy-goto-char-2)
           ("C-c g l"    . avy-goto-line)
           ( "C-c g w"   . avy-goto-word-0)))
 
