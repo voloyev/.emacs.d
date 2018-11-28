a55917afe2fe617711f5ebaab8355f544d265b14
Volodymyr Yevtushenko <voloyev@gmail.com>
2018-11-28 12:51:50 +0200

    Add keybinding to activate evil mode

--- init.el
+++ init.el
@@ -294,7 +294,8 @@
 (use-package evil
      :ensure t
      :bind
-     (("C-c e e" . evil-local-mode)))
+     (("C-c e e" . evil-local-mode)
+      ("<f2> <f2>" . evil-mode)))
 
 (use-package evil-matchit
     :ensure t)
