9331a18dd19df0608533f7ce0307437739aea406
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2018-12-24 14:11:31 +0200

    Fix keybindings

--- modules/avy-module.el
+++ modules/avy-module.el
@@ -5,7 +5,7 @@
 (use-package avy
     :ensure t
     :bind
-    ("C-;" . avy-goto-char)
+    ("C-c ;" . avy-goto-char)
     ("C-c :" . avy-goto-char-2)
     ("C-c g l" . avy-goto-line)
     ("C-c g w" . avy-goto-word-0))
