be23b77f6b4e97e77b4b06877ec0122b39f8e242
Volodymyr Yevtushenko <voloyev@gmail.com>
2018-12-26 18:03:42 +0200

    Update lsp

--- modules/lsp-module.el
+++ modules/lsp-module.el
@@ -1,18 +1,25 @@
 (use-package lsp-mode
     :commands lsp
-    :ensure t
-    :hook (ruby-mode)
-    :hook (js2-mode)
-    :hook (js2-jsx-mode)
-    :hook (vue-mode))
+    :ensure t)
+
+(require 'lsp-clients)
+
+(add-hook 'ruby-mode 'lsp)
+(add-hook 'js2-mode-hook 'lsp)
+(add-hook 'js2-jsx-mode 'lsp)
+(add-hook 'vue-mode 'lsp)
 
 (use-package lsp-ui
     :commands lsp-ui-mode
     :ensure t)
 
+(add-hook 'lsp-mode-hook 'lsp-ui-mode)
+
 (use-package company-lsp
     :commands company-lsp
     :ensure t)
+
+(push 'company-lsp company-backends)
 
 (provide 'lsp-module)
 ;;; lsp-module ends here
