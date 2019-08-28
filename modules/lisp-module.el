9c96dba540b5fa483a48fd03e4f49cb477b2ea3e
Volodymyr Yevtushenko <voloyev@gmail.com>
2019-08-28 14:47:41 +0300

    Change gitmodule

--- modules/lisp-module.el
+++ modules/lisp-module.el
@@ -9,12 +9,6 @@
     :init
     (setq inferior-lisp-program "ros -Q run"))
 
-;; (use-package slime
-;;     :ensure t
-;;     :init
-;;     (setq inferior-lisp-program "ros -Q run")
-;;     (setq slime-contribs '(slime-fancy slime-company)))
-
 (use-package elisp-slime-nav
     :ensure t
     :init
@@ -24,23 +18,26 @@
 (use-package racket-mode
     :ensure t)
 
-(use-package paredit
+(use-package smartparens
     :ensure t
-    :hook(paredit-mode . lisp-mode)
-    :hook(paredit-mode . emacs-lisp-mode)
-    :hook(paredit-mode . clojure-mode)
-    :hook(paredit-mode . racket-mode)
-    :hook(paredit-mode . slime-mode)
-    :hook(paredit-mode . cider-mode))
+    :hook(smartparens-strict-mode . lisp-mode)
+    :hook(smartparens-strict-mode . emacs-lisp-mode)
+    :hook(smartparens-strict-mode . clojure-mode)
+    :hook(smartparens-strict-mode . racket-mode)
+    :hook(smartparens-strict-mode . slime-mode)
+    :hook(smartparens-strict-mode . sly-mode)
+    :hook(smartparens-strict-mode . cider-mode))
 
 (use-package lispy
     :ensure t
     :bind(("C-c C-v l" . lispy-mode)))
 
-(add-hook 'lisp-mode-hook 'slime-lisp-mode-hook)
-
 (use-package geiser
-             :init
-             (setq geiser-default-implementation 'guile))
+    :init
+  (setq geiser-default-implementation 'racket))
+
+
+(add-hook 'lisp-mode-hook 'sly-editing-mode)
+
 (provide 'lisp-module)
 ;;; lisp-module ends here
