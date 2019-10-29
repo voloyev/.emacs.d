ce673797f3bc310845357ca213669add9daa939e
Volodymyr Yevtushenko <voloyev@vivaldi.net>
2019-10-29 15:44:13 +0200

    Fix elm module

--- modules/js-module.el
+++ modules/js-module.el
@@ -37,12 +37,10 @@
     :mode ("\\.jsx\\'" . js-mode)
     :hook (j2-minore-mode . js-mode))
 
-(use-package elm-mode
-    :ensure t
-    :mode ("\\.elm\\'" . elm-mode)
-    :config (elm-format-on-save-mode t)
-    :hook (smartparens-mode))
-
+;; (use-package elm-mode
+;;     :ensure t
+;;     :config (setq elm-format-on-save t)
+;;     :hook (smartparens-mode . elm-mode))
 (custom-set-variables '(coffee-tab-width 2))
 (custom-set-variables '(js2-basic-offset 2))
 (custom-set-variables '(js-basic-offset 2))
@@ -57,8 +55,13 @@
 ;; aligns annotation to the right hand side
 (setq company-tooltip-align-annotations t)
 
+;; elm-mode
+(add-hook 'elm-mode-hook 'smartparens-mode-hook)
+(setq elm-format-on-save t)
 ;; tern setup
 (add-to-list 'company-backends 'company-tern)
+(add-to-list 'company-backends 'company-elm)
+;; (setq elm-format-on-save t)
 
 (provide 'js-module)
 ;;; js-module.el ends here
