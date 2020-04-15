7f192a94896c90f44ae629e96bbc847abf141b61
Volodymyr Yevtushenko <voloyev@pm.me>
2020-04-15 12:16:24 +0300

    Add config for macos

--- init.el
+++ init.el
@@ -412,11 +412,18 @@
 ;; themes
 ;; (setq custom-safe-themes t)
 ;; (add-to-list 'custom-theme-load-path "~/workspace/lisp/emacs-lisp/sexy-monochrome-theme")
-(use-package sexy-monochrome-theme
+;; (use-package sexy-monochrome-theme
+;;     :ensure t
+;;     :init
+;;     (load-theme 'sexy-monochrome t)
+;;     (enable-theme 'sexy-monochrome))
+
+
+(use-package zenburn-theme
     :ensure t
     :init
-    (load-theme 'sexy-monochrome t)
-    (enable-theme 'sexy-monochrome))
+    (load-theme 'zenburn t)
+    (enable-theme 'zenburn))
 
 (use-package lispy
     :ensure t
